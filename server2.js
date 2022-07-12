57fecac0fbe1fdfc1af5ebcbaca6c9aeabd94264
Ubuntu <ubuntu@vps-9f62e310.vps.ovh.net>
2022-07-12 13:16:43 +0000

    reverting remote repository

--- server2.js
+++ server2.js
@@ -1,6 +1,6 @@
 
 const app = require('./app');
-app.set('port', 8084);
+app.set('port', 8083);
 
 const server2 = app.listen(app.get('port'), () => {
 		console.log(`I'am setted on port ${ server2.address().port }`);
